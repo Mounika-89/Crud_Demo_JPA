2c1d749f0972da384feb406a1beb9fb76c2ce01a
Mounika-89 <mounikapmpalthady60081@gmail.com>
2023-03-13 22:27:21 +0530

    first crud demo jpa commit

---
+++ src/main/java/com/myspring/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,17 @@
+package com.myspring.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import com.myspring.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeDAO {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+	
+}
